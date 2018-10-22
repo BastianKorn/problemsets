513e73993c3d3613cc927b291d2c1fef3d48ae83
BastianKorn <43954499+BastianKorn@users.noreply.github.com>
2018-10-22 15:33:02 +0200

    Add files via upload

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -6,34 +6,36 @@
 
 int main(int argc, char* argv[])
 {
-    int inputKey = atoi(argv[1]);
 
     if (argc == 2)
     {
+        int inputKey = atoi(argv[1]);
         if(inputKey!=0)
         {
-            printf("%i\n", inputKey); 
-            string plaintext = get_string("plaintext: \n");
-        
-                for(int i=0;i<strlen(plaintext);i++)
+            string plaintext = get_string("plaintext: ");
+
+            printf("ciphertext: ");
+            for(int i=0;i<strlen(plaintext);i++)
+            {
+                if((plaintext[i] >= 'a' && plaintext[i] <= 'z') || (plaintext[i] >= 'A' && plaintext[i] <= 'Z') )
                 {
-                    if((plaintext[i] >= 'a' && plaintext[i] <= 'z') || (plaintext[i] >= 'A' && plaintext[i] <= 'Z') )
+                    if(isupper(plaintext[i]))
                     {
-                        if(isupper(plaintext[i]))
+                        int c = (plaintext[i] - 65 + inputKey) % 26;
+                        printf("%c", (c+65));
+                    }else if(islower(plaintext[i]))
                         {
-                            int c = (plaintext[i] - 65 + inputKey) % 26;
-                            printf("%c", (c+65));
-                        }else if(islower(plaintext[i]))
-                             {
-                            int c = (plaintext[i] - 97 + inputKey) % 26;
-                                printf("%c", (c+97));
-                             }
-                    }else
-                        return 1;
-                }
+                        int c = (plaintext[i] - 97 + inputKey) % 26;
+                        printf("%c", (c+97));
+                        }
+                }else
+                    return 1;
+            }
             printf("\n");
         }else
-            printf("Usage: ./caesar key\n"); 
+            printf("Usage: ./caesar key\n");
+            return 2;
     }else
-        printf("Usage: ./caesar key\n"); 
+        printf("Usage: ./caesar key\n");
+        return 3;
 }
--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -3,10 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
- int main(int argc, string argv[])
- {
-    int keylength = strlen(argv[1]);
-    string keyword = argv[1];
+int main(int argc, string argv[])
+{
 
     if (argc != 2)
     {
@@ -14,19 +12,24 @@
         return 1;
     }
 
+    int keylength = strlen(argv[1]);
+    int j = 0;
+    string keyword = argv[1];
+
     for (int i = 0; i < keylength; i++)
     {
-        if (isalpha(keyword[i]) == 0)
+        if (!isalpha(keyword[i]))
         {
             printf("Alphabetical keys only!\n");
             return 1;
         }
     }
 
-    string plaintext = get_string("Plaintext: ");
-    int j = 0;
+    string plaintext = get_string("plaintext: ");
 
-    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    printf("ciphertext: ");
+
+    for (int i = 0; i < strlen(plaintext); i++)
     {
         j = j % keylength;
 
@@ -35,19 +38,18 @@
             if (islower(plaintext[i]) && islower(keyword[j]))
             {
                 printf("%c", (((plaintext[i] - 97) + (keyword[j] - 97)) % 26) + 97);
-            } else if (isupper(plaintext[i]) && islower(keyword[j]))
+            }else if (isupper(plaintext[i]) && islower(keyword[j]))
             {
                 printf("%c", (((plaintext[i] - 65) + (keyword[j] - 97)) % 26) + 65);
-            } else if (islower(plaintext[i]) && isupper(keyword[j]))
+            }else if (islower(plaintext[i]) && isupper(keyword[j]))
             {
                 printf("%c", (((plaintext[i] - 97) + (keyword[j] - 65)) % 26) + 97);
-            } else if (isupper(plaintext[i]) && isupper(keyword[j]))
+            }else if (isupper(plaintext[i]) && isupper(keyword[j]))
             {
                 printf("%c", (((plaintext[i] - 65) + (keyword[j] - 65)) % 26) + 65);
             }
             j++;
-        }
-        else
+        }else
         {
             printf("%c", plaintext[i]);
         }
