25bd32e4496d576212b5a2b0ff48a2c1e4b85c76
BastianKorn <43954499+BastianKorn@users.noreply.github.com>
2018-11-09 13:47:24 +0100

    Add files via upload

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -6,36 +6,28 @@
 
 int main(int argc, char* argv[])
 {
-
-    if (argc == 2)
-    {
+    if (argc == 2) {
         int inputKey = atoi(argv[1]);
-        if(inputKey!=0)
-        {
+        if(inputKey != 0) {
             string plaintext = get_string("plaintext: ");
-
             printf("ciphertext: ");
-            for(int i=0;i<strlen(plaintext);i++)
-            {
-                if((plaintext[i] >= 'a' && plaintext[i] <= 'z') || (plaintext[i] >= 'A' && plaintext[i] <= 'Z') )
-                {
-                    if(isupper(plaintext[i]))
-                    {
+            for(int i=0;i<strlen(plaintext);i++) {
+                if((plaintext[i] >= 'a' && plaintext[i] <= 'z') || (plaintext[i] >= 'A' && plaintext[i] <= 'Z') ) {
+                    if(isupper(plaintext[i])) {
                         int c = (plaintext[i] - 65 + inputKey) % 26;
                         printf("%c", (c+65));
-                    }else if(islower(plaintext[i]))
-                        {
+                    } else if(islower(plaintext[i])) {
                         int c = (plaintext[i] - 97 + inputKey) % 26;
                         printf("%c", (c+97));
-                        }
+                    }
                 }else
                     return 1;
             }
             printf("\n");
-        }else
+        } else
             printf("Usage: ./caesar key\n");
             return 2;
-    }else
+    } else
         printf("Usage: ./caesar key\n");
         return 3;
 }
--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -5,9 +5,7 @@
 
 int main(int argc, string argv[])
 {
-
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Usage: ./vigenere key\n");
         return 1;
     }
@@ -16,41 +14,30 @@ int main(int argc, string argv[])
     int j = 0;
     string keyword = argv[1];
 
-    for (int i = 0; i < keylength; i++)
-    {
-        if (!isalpha(keyword[i]))
-        {
+    for (int i = 0; i < keylength; i++) {
+        if (!isalpha(keyword[i])) {
             printf("Alphabetical keys only!\n");
             return 1;
         }
     }
 
     string plaintext = get_string("plaintext: ");
-
     printf("ciphertext: ");
 
-    for (int i = 0; i < strlen(plaintext); i++)
-    {
+    for (int i = 0; i < strlen(plaintext); i++) {
         j = j % keylength;
-
-        if (isalpha(plaintext[i]))
-        {
-            if (islower(plaintext[i]) && islower(keyword[j]))
-            {
+        if (isalpha(plaintext[i])) {
+            if (islower(plaintext[i]) && islower(keyword[j])) {
                 printf("%c", (((plaintext[i] - 97) + (keyword[j] - 97)) % 26) + 97);
-            }else if (isupper(plaintext[i]) && islower(keyword[j]))
-            {
+            } else if (isupper(plaintext[i]) && islower(keyword[j])) {
                 printf("%c", (((plaintext[i] - 65) + (keyword[j] - 97)) % 26) + 65);
-            }else if (islower(plaintext[i]) && isupper(keyword[j]))
-            {
+            } else if (islower(plaintext[i]) && isupper(keyword[j])) {
                 printf("%c", (((plaintext[i] - 97) + (keyword[j] - 65)) % 26) + 97);
-            }else if (isupper(plaintext[i]) && isupper(keyword[j]))
-            {
+            } else if (isupper(plaintext[i]) && isupper(keyword[j])) {
                 printf("%c", (((plaintext[i] - 65) + (keyword[j] - 65)) % 26) + 65);
             }
             j++;
-        }else
-        {
+        } else {
             printf("%c", plaintext[i]);
         }
     }
